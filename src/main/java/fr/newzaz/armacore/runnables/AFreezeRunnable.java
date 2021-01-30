778a9dc65af1497e6e53df4fcc331d3ec3720c80
BuildTools <unconfigured@null.spigotmc.org>
2021-01-30 14:12:29 +0100

    Update PlayerListener.java

--- src/main/java/fr/newzaz/armacore/runnables/AFreezeRunnable.java
+++ src/main/java/fr/newzaz/armacore/runnables/AFreezeRunnable.java
@@ -1,19 +1,20 @@
 package fr.newzaz.armacore.runnables;
 
 import fr.newzaz.armacore.commands.AModerationCommand;
+import fr.newzaz.armacore.data.PlayerListener;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class AFreezeRunnable extends BukkitRunnable {
     Player p;
-
+    PlayerListener pl = new PlayerListener();
     public AFreezeRunnable(Player p) {
         this.p = p;
     }
 
     @Override
     public void run() {
-        if (AModerationCommand.FreezeUUID.contains(p.getUniqueId())) {
+        if (pl.getFreezeUUID().contains(p.getUniqueId())) {
             this.p.sendMessage("§cVous êtes freeze venez discord : https://discord.gg/gdk5jAG");
         } else {
             this.cancel();
