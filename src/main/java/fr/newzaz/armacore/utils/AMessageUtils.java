cf04e6fcc5e54ddd4aff2c2e232c1ea8aa534a11
BuildTools <unconfigured@null.spigotmc.org>
2021-02-01 20:10:57 +0100

    add final on AMessageUtils.java, APermissionUtils.java

--- src/main/java/fr/newzaz/armacore/utils/AMessageUtils.java
+++ src/main/java/fr/newzaz/armacore/utils/AMessageUtils.java
@@ -2,8 +2,8 @@ package fr.newzaz.armacore.utils;
 
 public class AMessageUtils {
 
-    public static String MESSAGE_BREAK = "§cTu n'as pas la permission pour faire ceci (§4"+ APermissionUtils.PERMISSION_BREAK+"§c)";
-    public static String MESSAGE_PLACE = "§cTu n'as pas la permission pour faire ceci (§4"+ APermissionUtils.PERMISSION_PLACE+"§c)";
-    public static String MESSAGE_INSTANCEPLAYER = "§cTu doit être un joueur pour faire cette commande !";
-    public static String MESSAGE_NOPERMISSION = "§cTu n'as pas la permission de faire ceci ! [%permission%]";
+    public static final String MESSAGE_BREAK = "§cTu n'as pas la permission pour faire ceci (§4"+ APermissionUtils.PERMISSION_BREAK+"§c)";
+    public static final String MESSAGE_PLACE = "§cTu n'as pas la permission pour faire ceci (§4"+ APermissionUtils.PERMISSION_PLACE+"§c)";
+    public static final String MESSAGE_INSTANCEPLAYER = "§cTu doit être un joueur pour faire cette commande !";
+    public static final String MESSAGE_NOPERMISSION = "§cTu n'as pas la permission de faire ceci ! [%permission%]";
 }
--- src/main/java/fr/newzaz/armacore/utils/APermissionUtils.java
+++ src/main/java/fr/newzaz/armacore/utils/APermissionUtils.java
@@ -2,15 +2,15 @@ package fr.newzaz.armacore.utils;
 
 public class APermissionUtils {
 
-    public static String PERMISSION_ALL = "ArmaCore.*";
-    public static String PERMISSION_BREAK = "ArmaCore.break";
-    public static String PERMISSION_PLACE = "ArmaCore.place";
-    public static String PERMISSION_GIVE_MONEY = "ArmaCore.giveMoney";
-    public static String PERMISSION_RESET_MONEY = "ArmaCore.resetMoney";
-    public static String PERMISSION_MODERATION = "ArmaCore.moderation";
-    public static String PERMISSION_GAMEMODE_CREATIVE = "ArmaCore.gamemode.creative";
-    public static String PERMISSION_GAMEMODE_SPECTATEUR = "ArmaCore.gamemode.spectateur";
-    public static String PERMISSION_GAMEMODE_SURVIVAL = "ArmaCore.gamemode.survival";
-    public static String PERMISSION_FLY = "ArmaCore.fly";
-    public static String PERMISSION_VANISH = "ArmaCore.vanish";
+    public static final String PERMISSION_ALL = "ArmaCore.*";
+    public static final String PERMISSION_BREAK = "ArmaCore.break";
+    public static final String PERMISSION_PLACE = "ArmaCore.place";
+    public static final String PERMISSION_GIVE_MONEY = "ArmaCore.giveMoney";
+    public static final String PERMISSION_RESET_MONEY = "ArmaCore.resetMoney";
+    public static final String PERMISSION_MODERATION = "ArmaCore.moderation";
+    public static final String PERMISSION_GAMEMODE_CREATIVE = "ArmaCore.gamemode.creative";
+    public static final String PERMISSION_GAMEMODE_SPECTATEUR = "ArmaCore.gamemode.spectateur";
+    public static final String PERMISSION_GAMEMODE_SURVIVAL = "ArmaCore.gamemode.survival";
+    public static final String PERMISSION_FLY = "ArmaCore.fly";
+    public static final String PERMISSION_VANISH = "ArmaCore.vanish";
 }
